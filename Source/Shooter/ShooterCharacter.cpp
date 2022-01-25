2eab3ffcb5874698e51d6631f82a987abcad61d6
Joe Horacki <prince_937@hotmail.com>
2022-01-25 11:03:59 -0600

    10. UE_LOG format specifiers

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -19,8 +19,24 @@ void AShooterCharacter::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("BeginPlay has been called"));
 
 	int myInt{ 42 };
+	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
 
-	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt)
+	float myFloat{ 3.14159f };
+	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
+
+	double myDouble{ 0.000756 };
+	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
+
+	char myChar{ 'J' };
+	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
+
+	wchar_t wideChar{ L'J' };
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t wideChar: %lc"), wideChar);
+
+	bool myBool{ true };
+	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
+
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"), myInt, myFloat, false);
 }
 
 // Called every frame
