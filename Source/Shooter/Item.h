412b5e97e0ba40f8e169a5571ac1c502ae395f94
Joe Horacki <prince_937@hotmail.com>
2022-02-05 19:21:39 -0600

    66. Bind Item Name

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -49,6 +49,10 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	class USphereComponent* AreaSphere;
 
+	// The name which appears on the pickup Widget
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	FString ItemName;
+
 public:
 
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
