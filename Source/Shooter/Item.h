529ae8c9fa23b1a0db2c42c82d09d3cff4cd606e
Joe Horacki <prince_937@hotmail.com>
2022-02-08 13:46:30 -0600

    81. Item Z Curve

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -98,7 +98,9 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	EItemState ItemState;
 
-
+	// The curve asset to use for the item's Z location when interping
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UCurveFloat* ItemZCurve;
 
 public:
 
